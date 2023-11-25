0af4d73c3101b3a23ca05fb70553994845729e8d
Pear15 <151445039+ColourlessPear15@users.noreply.github.com>
2023-11-25 21:47:37 +0000

    Finish Sword combat :D

--- HackSussex/Assets/PlayerCombat.cs
+++ HackSussex/Assets/PlayerCombat.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animation;
+    public BoxCollider2D collision;
+    private float cooldown;
+
+
+
+
+    void Attack()
     {
-        
+        // Play an Attack animation
+        // Detect enemies in range of attack
+        // Damage them
+        animation.SetTrigger("Swing");
+        collision.enabled = true;
+        cooldown = 0.016f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cooldown -= Time.deltaTime;
+
+        if (cooldown <= 0)
+        {
+            collision.enabled = false;
+        }
+
+         if (Input.GetKeyDown(KeyCode.F) && cooldown <= 0)
+    {
+        Attack();
+    }
+
     }
 }
