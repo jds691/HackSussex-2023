0cde0555617b6f782c4d0654378523bf6ab821ce
Neo <52429194+jds691@users.noreply.github.com>
2023-11-26 09:58:03 +0000

    Create basic level transition system

---
+++ HackSussex/Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class LevelTransition : MonoBehaviour
+{
+    [SerializeField]
+    private string _sceneName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log("Player reached end of level");
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
